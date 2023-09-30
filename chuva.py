7b437d7e6b1436fe99bbeed9c0873ae3a553bb88
Lucas Anjos <oargus.g@gmail.com>
2023-09-30 12:57:53 -0300

    Animação de andando do personagem

--- chuva.py
+++ chuva.py
@@ -8,6 +8,9 @@ pygame.init()
 tamanho = (960, 540)
 tela = pygame.display.set_mode(tamanho)
 
+# Define o Titulo da Janela
+pygame.display.set_caption("ChuvaMortal")
+
 ##
 ## Importa os arquivos necessários
 ##
@@ -32,17 +35,29 @@ fundo_chao = pygame.transform.scale(fundo_chao, tamanho)
 fundo_lua = pygame.transform.scale(fundo_lua, tamanho)
 fundo_rochas_voadoras = pygame.transform.scale(fundo_rochas_voadoras, tamanho)
 
-# Importa o personagem
-jogador_parado_surf = pygame.image.load('assets/jogador/parado/Hero Boy Idle1.png').convert_alpha()
-jogador_parado_rect = jogador_parado_surf.get_rect( midbottom = (100, 530))
+# Carrega as imagens do personagem
+jogador_index = 0
+jogador_parado_superficies = []
+jogador_voando_superficies = []
 
-# Define o Titulo da Janela
-pygame.display.set_caption("ChuvaMortal")
+# Carrega o jogador parado
+for imagem in range(1, 14):
+    img = pygame.image.load(f'assets/jogador/parado/Hero Boy Idle{imagem}.png').convert_alpha()
+    jogador_parado_superficies.append(img)
+
+# Carrega o jogador se movimentando
+for imagem in range(1, 9):
+    img = pygame.image.load(f'assets/jogador/voar/Hero Boy Fly{imagem}.png').convert_alpha()
+    jogador_voando_superficies.append(img)
+
+jogador_retangulo = jogador_parado_superficies[jogador_index].get_rect( center = (100, 430))
 
 # Cria um relógio para controlar os FPS
 relogio = pygame.time.Clock()
 
+# Controla se o personagem está andando (negativo esquerda, positivo direita)
 movimento_personagem = 0
+
 # Loop principal do jogo
 while True:
     for evento in pygame.event.get():
@@ -57,6 +72,13 @@ while True:
             if evento.key == pygame.K_LEFT:
                 movimento_personagem = -5
 
+        if evento.type == pygame.KEYUP:
+            if evento.key == pygame.K_RIGHT:
+                movimento_personagem = 0
+
+            if evento.key == pygame.K_LEFT:
+                movimento_personagem = 0
+
     # Desenha o fundo na tela
     tela.blit(plano_fundo, (0, 0))
     tela.blit(fundo_estrelas, (0, 0))
@@ -67,9 +89,22 @@ while True:
     tela.blit(fundo_lua, (0, 0))
     tela.blit(fundo_rochas_voadoras, (0, 0))
     
+    # Calcula o movimento do personagem
+    jogador_retangulo.x += movimento_personagem
+    # jogador_surface = null
+
+    if movimento_personagem == 0: # Jogador está parado
+        jogador_superficies = jogador_parado_superficies
+    else: # Jogador está se movimentando
+        jogador_superficies = jogador_voando_superficies
+
     # Desenha o jogador na tela
-    jogador_parado_rect.x += movimento_personagem
-    tela.blit(jogador_parado_surf, jogador_parado_rect)
+    tela.blit(jogador_superficies[int(jogador_index)], jogador_retangulo)
+
+    jogador_index += 0.11
+
+    if jogador_index > len(jogador_superficies) - 1:
+        jogador_index = 0
 
     # Atualiza a tela com o conteudo
     pygame.display.update()
